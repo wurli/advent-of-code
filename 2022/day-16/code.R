ed734b348e4b619743e9130acf58a9971cf523a3
Jacob Scott <jacob.scott@education.gov.uk>
2022-12-16 19:30:23 +0000

    tidying

--- 2022/day-16/code.R
+++ 2022/day-16/code.R
@@ -7,7 +7,6 @@ input <- lapply(input, `[`, -1)
 nodes <- names(input[sapply(input, `[[`, "rate") > 0])
 
 all_links <- function(node, len = 1, to_find = nodes, covered = NULL) {
-  
   covered  <- union(covered, node) 
   to_find  <- setdiff(to_find, node) 
   links    <- input[[node]]$path
@@ -19,11 +18,10 @@ all_links <- function(node, len = 1, to_find = nodes, covered = NULL) {
   
   c(lapply(found, \(e) list(name = e, length = len)), 
     unlist(lapply(next_bit, all_links, len + 1, to_find, covered), recursive = FALSE))
-  
 }
 
 shortest_links <- function(node) {
-  links <- find_all_links(node) 
+  links <- all_links(node) 
   split(links, sapply(links, `[[`, "name")) |> 
     lapply(\(x) min(sapply(x, `[[`, "length"))) 
 }
@@ -35,7 +33,6 @@ all_paths <- function(start = list("AA"), max_len = 30) {
   pts <- lapply(start, \(p) {
     len <- length(p)
     if (len > 1) len <- len + sum(apply(embed(p, 2), 1, \(x) links[[rev(x)]]))
-    
     opts <- links[[tail(p, 1)]]
     opts <- opts[!names(opts) %in% p]
     opts <- opts[vapply(opts, \(x) len + x <= max_len, logical(1))]
@@ -53,12 +50,13 @@ all_paths <- function(start = list("AA"), max_len = 30) {
   
 }
 
-paths <- all_paths("AA")
-
 # # Best path
 # path <- c("AA", "DD", "BB", "JJ", "HH", "EE", "CC")
 
-max(sapply(paths, \(path) {
+# Part 1
+paths30 <- all_paths("AA")
+
+max(sapply(paths30, \(path) {
   
   rates <- sapply(path, \(p) input[[p]]$rate)
   times <- 31 - cumsum(c(0, apply(embed(path, 2), 1, \(x) links[[rev(x)]])) + 1)
@@ -66,3 +64,7 @@ max(sapply(paths, \(path) {
   
 }))
 
+# Part 2
+paths26 <- all_paths("AA", max_len = 26)
+
+nodes
