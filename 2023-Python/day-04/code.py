e0d942ddaf2945233296ece9667491eeb5a5cd40
Jacob Scott <jake12358@gmail.com>
2023-12-04 11:43:33 +0000

    another minor refactor

--- 2023-Python/day-04/code.py
+++ 2023-Python/day-04/code.py
@@ -1,6 +1,6 @@
-import re
-import math
 import numpy as np
+import math
+import re
 
 with open("2023-Python/day-04/input.txt") as input_file:
     input = [re.sub(r"\s+", " ", line) for line in input_file.readlines()]
@@ -8,16 +8,15 @@ with open("2023-Python/day-04/input.txt") as input_file:
 input_parsed = [
     {
         "Card_No": int(re.findall(r"(?<=Card )\d+", line)[0]),
-        "Winning": set(map(int, re.split(r"\s+", re.findall(r"(?<=: ).+(?= \|)", line)[0]))),
-        "Present": set(map(int, re.split(r"\s+", re.findall(r"(?<=\| ).+(?= )", line)[0])))
+        "Winning": {int(x) for x in re.split(r"\s+", re.findall(r"(?<=: ).+(?= \|)", line)[0])},
+        "Present": {int(x) for x in re.split(r"\s+", re.findall(r"(?<=\| ).+(?= )", line)[0])},
     }
     for line in input
 ]
 
-# -- Part 1 ------------------------------------------------------------------------------------
+# -- Part 1 -------------------------------------------------------------------------------------
 n_matches = [len(card["Winning"].intersection(card["Present"])) for card in input_parsed]
-scores = [math.floor(2 ** (n - 1)) for n in n_matches]
-result1 = sum(scores)
+result1 = sum([math.floor(2 ** (n - 1)) for n in n_matches])
 
 # -- Part 2 -------------------------------------------------------------------------------------
 n_scratchcards = np.repeat(1, len(input_parsed))
