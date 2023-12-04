98a317740ff1cb2b121844fd9bd9717efe9a6d7b
Jacob Scott <jake12358@gmail.com>
2023-12-04 12:41:25 +0000

    renaming

--- 2023-Python/day-04/code.py
+++ 2023-Python/day-04/code.py
@@ -5,7 +5,7 @@ import re
 with open("2023-Python/day-04/input.txt") as input_file:
     input = input_file.readlines()
 
-input_parsed = [
+cards = [
     {
         "Winning": set(map(int, re.findall(r"\d+", re.findall(r":.+\|", line)[0]))),
         "Present": set(map(int, re.findall(r"\d+", re.findall(r"\|.+$", line)[0])))
@@ -13,13 +13,13 @@ input_parsed = [
     for line in input
 ]
 
-# -- Part 1 -------------------------------------------------------------------------------------
-n_matches = [len(card["Winning"].intersection(card["Present"])) for card in input_parsed]
+# -- Part 1 ---------------------------------------------------------------------------
+n_matches = [len(card["Winning"].intersection(card["Present"])) for card in cards]
 result1 = sum([math.floor(2 ** (n - 1)) for n in n_matches])
 
-# -- Part 2 -------------------------------------------------------------------------------------
-n_scratchcards = np.repeat(1, len(input_parsed))
+# -- Part 2 ---------------------------------------------------------------------------
+n_cards = np.repeat(1, len(cards))
 for card, matches in enumerate(n_matches):
-    n_scratchcards[range(card + 1, card + 1 + matches)] += n_scratchcards[card] 
+    n_cards[range(card + 1, card + 1 + matches)] += n_cards[card] 
 
-result2 = sum(n_scratchcards)
+result2 = sum(n_cards)
