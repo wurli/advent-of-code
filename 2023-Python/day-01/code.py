a6d569427dfdc36de9bc13be6c9e784da9254f09
Jacob Scott <jake12358@gmail.com>
2023-12-05 10:09:34 +0000

    add soln for 2023.1

--- 2023-Python/day-01/code.py
+++ 2023-Python/day-01/code.py
@@ -12,23 +12,23 @@ def first_and_last_num(x: str):
 result1 = sum([first_and_last_num(line) for line in input])
 
 # Part 2 ------------------------------------------------------
-nums = {
+subs1 = {
+    "one"  : "onee"  , "two"  : "twoo"  , "three": "threee",
+    "four" : "fourr" , "five" : "fivee" , "six"  : "sixx",
+    "seven": "sevenn", "eight": "eightt", "nine" : "ninee"
+}
+
+subs2 = {
     "one"  : 1, "two"  : 2, "three": 3, 
     "four" : 4, "five" : 5, "six"  : 6, 
     "seven": 7, "eight": 8, "nine" : 9,
-    "twone": 21,
-    "sevenine": 79,
-    "eightwo": 82,
-    "eightwone": 821
 }
 
-def multi_replace(x, replacements):
-    pattern = "|".join(nums.keys())
-    def switch(match):
-        return str(replacements[match[0]])
-    return re.sub(pattern, switch, x)
+def replace(x, replacements):
+    for (pattern, replacement) in replacements.items():
+        x = x.replace(pattern, replacement)
+    return x
 
-input2 = [multi_replace(line, nums) for line in input]
-result2 = sum([first_and_last_num(line) for line in input2])
-
-# 54807 - too low
+input2a = [replace(line, subs1) for line in input]
+input2b = [replace(line, subs2) for line in input2a]
+result2 = sum([first_and_last_num(line) for line in input2b])
