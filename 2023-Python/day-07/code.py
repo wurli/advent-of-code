778ae06756211cef4acc33966622929f7cbe1e1c
Jacob Scott <jake12358@gmail.com>
2023-12-07 17:42:43 +0000

    actually rename vars

--- 2023-Python/day-07/code.py
+++ 2023-Python/day-07/code.py
@@ -5,7 +5,7 @@ with open("2023-Python/day-07/input.txt") as input_file:
 def hand_strength(hand):
     combo_strength = [sum([13**hand.count(card) for card in set(hand)])]
     card_strengths = ["23456789TJQKA".index(card) for card in hand]
-    return hand_strength + combo_strength
+    return combo_strength + card_strengths
 
 input.sort(key = lambda x: hand_strength(x[0]))
 result1 = sum([(pos + 1) * int(x[1]) for pos, x in enumerate(input)])
@@ -16,7 +16,7 @@ def hand_strength2(hand):
     card_counts[card_counts.index(max(card_counts))] += hand.count("J")
     combo_strength = [sum([13**count for count in card_counts])] 
     card_strengths = ["J23456789TQKA".index(card) for card in hand]
-    return hand_strength + combo_strength 
+    return combo_strength + card_strengths 
 
 input.sort(key = lambda x: hand_strength2(x[0]))
 result2 = sum([(pos + 1) * int(x[1]) for pos, x in enumerate(input)])
