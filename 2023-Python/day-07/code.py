bc3ae8c97c537c00457f3f8c435dbb97f457e736
Jacob Scott <jake12358@gmail.com>
2023-12-07 17:38:10 +0000

    add 2023.7

---
+++ 2023-Python/day-07/code.py
@@ -0,0 +1,23 @@
+with open("2023-Python/day-07/input.txt") as input_file:
+    input = [line.split() for line in input_file.readlines()]
+
+# -- Part 1 --------------------------------------------------------------------
+def overall_strength(hand):
+    hand_strength = [sum([13**hand.count(card) for card in set(hand)])]
+    card_strengths = ["23456789TJQKA".index(card) for card in hand]
+    return hand_strength + card_strengths
+
+input.sort(key = lambda x: overall_strength(x[0]))
+result1 = sum([(pos + 1) * int(x[1]) for pos, x in enumerate(input)])
+
+# -- Part 2 --------------------------------------------------------------------
+def overall_strength2(hand):
+    card_counts = [hand.count(card) for card in set(hand) if card != "J"] or [0]
+    card_counts[card_counts.index(max(card_counts))] += hand.count("J")
+    hand_strength = [sum([13**count for count in card_counts])] 
+    card_strengths = ["J23456789TQKA".index(card) for card in hand]
+    return hand_strength + card_strengths 
+
+input.sort(key = lambda x: overall_strength2(x[0]))
+result2 = sum([(pos + 1) * int(x[1]) for pos, x in enumerate(input)])
+
